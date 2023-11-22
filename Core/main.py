7ab012437d6607f4d712b550626e1a88541431b4
JungJae98 <wogus8880@gmail.com>
2023-11-22 23:37:18 +0900

    split으로 값 나누기

--- Core/main.py
+++ Core/main.py
@@ -14,12 +14,24 @@ conn = mysql.connector.connect(
 
 # 커서 생성
 cursor = conn.cursor()
+# query문 작성
+query = "INSERT INTO top10_up (test) VALUES (%s)"
+
+# 컬럼명
+global company
+global price
+global limit
+global change_price
+global percent
+
+
 
 # 웹 드라이버 초기화 (ChromDriver 사용)
 service = Service(r'C:\Users\JungJaeHyeon\Desktop\StockProject\Core\chromedriver.exe')
 driver = webdriver.Chrome(service=service)
 
 top10_up = []
+
 
 try:
     # 네이버 금융 페이지
@@ -29,7 +41,7 @@ try:
     # a_tag = TOP종목 내에 있는 상승 버튼
     a_tag = driver.find_element(By.CSS_SELECTOR, "ul.tab_area.sise_top1 li.tab2 a")
     a_tag.click() # a_tag 클릭
-    time.sleep(2)
+    time.sleep(1)
 
     # up_rows는 상승된 종목들을 가져옴
     up_rows = driver.find_elements(By.CSS_SELECTOR, 'tbody#_topItems2 tr.up')
@@ -39,14 +51,28 @@ try:
         # cell의 내용들을 join을 이용하여 한 줄로 출력, 구분자는 공백
         row_text = ' '.join([cell.text.replace('\n', ' ') for cell in cells])
         top10_up.append(row_text)
+    #print(top10_up)
 
-    print(top10_up)
+    for i in top10_up:
+        split_top10 = (i.split(" ",))
+        company = split_top10[0]
+        price = split_top10[1]
+        limit = split_top10[2]
+        change_price = split_top10[3]
+        percent = split_top10[4]
 
-    query = "INSERT INTO top10_up (test) VALUES (%s)"
-    data = ("test",)
+    print(company)
+    print(price)
+    print(limit)
+    print(change_price)
+    print(percent)
 
-    cursor.execute(query, data)
-    conn.commit()
+
+    # #데이터베이스에 삽입될 데이터
+    # data = (top10_up, )
+    # #데이터베이스에 데이터 삽입 후 커밋
+    # cursor.execute(query, data)
+    # conn.commit()
 
 finally:
     # 드라이버 종료
